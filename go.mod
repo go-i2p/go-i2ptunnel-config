62677fc6392c848acb9886d87d9279a1ce1fc584
eyedeekay <hankhill19580@gmail.com>
2025-02-02 00:20:20 -0500

    fix compile erors

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/go-i2p/go-i2ptunnel-config
 go 1.23.5
 
 require (
+	github.com/go-i2p/i2pkeys v0.33.92
 	github.com/magiconair/properties v1.8.9
 	github.com/urfave/cli v1.22.16
 	gopkg.in/yaml.v2 v2.4.0
@@ -11,4 +12,6 @@ require (
 require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.5 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
 )
--- lib/sam.go
+++ lib/sam.go
@@ -1,76 +1,83 @@
 package i2pconv
 
 import (
-    "github.com/go-i2p/i2pkeys"
-    "path/filepath"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/go-i2p/i2pkeys"
 )
 
 // SAMTunnel returns the I2P keys and SAM options for this tunnel configuration.
 // If PersistentKeys is true, keys will be stored in a SAMv3 compatible format.
 func (c *TunnelConfig) SAMTunnel() (*i2pkeys.I2PKeys, []string, error) {
-    var keys *i2pkeys.I2PKeys
-    var err error
-    
-    if c.PersistentKeys {
-        // Get default I2P keystore path
-        keystore, err := i2pkeys.I2PKeystorePath()
-        if err != nil {
-            return nil, nil, err
-        }
-        
-        // Load or create keys for this tunnel
-        keypath := filepath.Join(keystore, c.Name+".keys")
-        loadedKeys, err := i2pkeys.LoadKeys(keypath) 
-        if err == nil {
-            keys = &loadedKeys
-        } else {
-            // Create new keys if none exist
-            newKeys, err := i2pkeys.GenerateKeys()
-            if err != nil {
-                return nil, nil, err
-            }
-            keys = &newKeys
-            // Store the new keys
-            if err := i2pkeys.StoreKeysIncompat(newKeys, keypath); err != nil {
-                return nil, nil, err
-            }
-        }
-    }
+	var keys *i2pkeys.I2PKeys
 
-    // Generate SAM options from config
-    var opts []string
-    
-    // Process I2CP options
-    for k, v := range c.I2CP {
-        opts = append(opts, "i2cp."+k+"="+fmt.Sprint(v))
-    }
+	if c.PersistentKey {
+		// Get default I2P keystore path
+		keystore, err := os.Getwd()
+		if err != nil {
+			return nil, nil, err
+		}
 
-    // Process tunnel options 
-    for k, v := range c.Tunnel {
-        opts = append(opts, k+"="+fmt.Sprint(v))
-    }
+		// Load or create keys for this tunnel
+		keypath := filepath.Join(keystore, c.Name+".keys")
+		loadedKeys, err := i2pkeys.LoadKeys(keypath)
+		if err == nil {
+			keys = &loadedKeys
+		} else {
+			// Create new keys if none exist
+			newKeys, err := i2pkeys.NewDestination()
+			if err != nil {
+				return nil, nil, err
+			}
+			keys = newKeys
+			// Store the new
+			keyFile, err := os.Open(keypath)
+			if err != nil {
+				return nil, nil, err
+			}
+			if err := i2pkeys.StoreKeysIncompat(*newKeys, keyFile); err != nil {
+				return nil, nil, err
+			}
+		}
+	}
 
-    // Process inbound/outbound options
-    for k, v := range c.Inbound {
-        opts = append(opts, "inbound."+k+"="+fmt.Sprint(v))
-    }
-    for k, v := range c.Outbound {
-        opts = append(opts, "outbound."+k+"="+fmt.Sprint(v))
-    }
+	// Generate SAM options from config
+	var opts []string
 
-    // Ensure lease set encryption
-    if !hasOption(opts, "i2cp.leaseSetEncType") {
-        opts = append(opts, "i2cp.leaseSetEncType=4,0")
-    }
+	// Process I2CP options
+	for k, v := range c.I2CP {
+		opts = append(opts, "i2cp."+k+"="+fmt.Sprint(v))
+	}
 
-    return keys, opts, nil
+	// Process tunnel options
+	for k, v := range c.Tunnel {
+		opts = append(opts, k+"="+fmt.Sprint(v))
+	}
+
+	// Process inbound/outbound options
+	for k, v := range c.Inbound {
+		opts = append(opts, "inbound."+k+"="+fmt.Sprint(v))
+	}
+	for k, v := range c.Outbound {
+		opts = append(opts, "outbound."+k+"="+fmt.Sprint(v))
+	}
+
+	// Ensure lease set encryption
+	if !hasOption(opts, "i2cp.leaseSetEncType") {
+		opts = append(opts, "i2cp.leaseSetEncType=4,0")
+	}
+
+	return keys, opts, nil
 }
 
 func hasOption(opts []string, prefix string) bool {
-    for _, opt := range opts {
-        if strings.HasPrefix(opt, prefix) {
-            return true
-        }
-    }
-    return false
+	for _, opt := range opts {
+		if strings.HasPrefix(opt, prefix) {
+			return true
+		}
+	}
+	return false
 }
